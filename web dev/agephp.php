13861c00a673afaeb569a5d7fe800c5ad998c730
Shiva <2230@tkmce.ac.in>
2023-01-18 23:30:29 +0530

    modified files

--- web dev/agephp.php
+++ web dev/agephp.php
@@ -1,21 +1,35 @@
 <?php
-   $localhost='localhost' ;
-   $user='root';
-   $pass='';
-   $db='age';
+$localhost = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'age';
 
-   $conn=mysqli_connect($localhost,$user,$pass,$db);
-   if(isset($_POST["submit"])){
-    $name=$_POST["name"];
-    $age = $_POST["age"];
-   }
-    if(conn){
-    $query = "INSERT INTO `agelist`(`sl  no`, `name`, `age`) VALUES ('','$name','$age')";
-        mysqli_query($conn,$query);
-    }
-    else{
-    print("Connection failed!");
-    }
+$conn = mysqli_connect($localhost, $user, $pass, $db);
+
+if (isset($_POST["submit"])) {
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    
+    if ($conn) {
+        $query = "INSERT INTO `agelist`(`slno`, `name`, `age`) VALUES (' ','$name','$age')";
+        $result=mysqli_query($conn, $query);
+    } else {
+        print("Connection failed!");
+    }}
+if(isset($_POST["delete"])){
+    $sql="DELETE FROM `agelist` WHERE slno=1";
+    $result = mysqli_query($conn, $sql);
+
+}
+$sql = "select * from agelist";
+$result = mysqli_query($conn, $sql);
+echo "<table>";
+echo "<tr><th>Name</th><th>Age</th></tr>";
+foreach ($result as $row) {
+    echo "<tr><td>" . $row['name']."</td><td>".$row['age']."</td></tr>";
+}
+echo "</table>"
+
 
 
 ?>
